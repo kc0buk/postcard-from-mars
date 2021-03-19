fec839d213074348fc3f8ef96e392e78312e8d43
Jeremiah Trnka <jeremiah@jeremiahtrnka.com>
2021-03-18 17:37:13 -0700

    Removed unused files.

--- src/constants/index.js
+++
@@ -1,2 +0,0 @@
-export const API_KEY = `${process.env.REACT_APP_API_KEY}`
-export const API_URL = `${process.env.REACT_APP_API_URI}`
--- src/utils/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-import { API_URL } from '../constants'
-
-
-export const fetchData = () => {
-    return axios.create({
-        baseURL: API_URL,
-    })
-}
