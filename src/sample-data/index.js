d5403403e0a327468bd4d58ec8b043a7e3d145c0
Jeremiah Trnka <jeremiah@jeremiahtrnka.com>
2021-03-19 15:49:50 -0700

    Added sample API data.

---
+++ src/sample-data/index.js
@@ -0,0 +1,349 @@
+// This is an example of the data returned by the NASA API from
+// https://api.nasa.gov/mars-photos/api/v1/rovers/perseverance/latest_photos?api_key=DEMO_KEY
+
+export const sampleData = {
+    "latest_photos": [
+        {
+            "id": 816847,
+            "sol": 26,
+            "camera": {
+                "id": 47,
+                "name": "SHERLOC_WATSON",
+                "rover_id": 8,
+                "full_name": "SHERLOC WATSON Camera"
+            },
+            "img_src": "https://mars.nasa.gov/mars2020-raw-images/pub/ods/surface/sol/00026/ids/edr/browse/shrlc/SI1_0026_0669252754_132ECM_N0030792SRLC07008_0000LUJ02_1200.jpg",
+            "earth_date": "2021-03-16",
+            "rover": {
+                "id": 8,
+                "name": "Perseverance",
+                "landing_date": "2021-02-18",
+                "launch_date": "2020-07-30",
+                "status": "active"
+            }
+        },
+        {
+            "id": 816851,
+            "sol": 26,
+            "camera": {
+                "id": 47,
+                "name": "SHERLOC_WATSON",
+                "rover_id": 8,
+                "full_name": "SHERLOC WATSON Camera"
+            },
+            "img_src": "https://mars.nasa.gov/mars2020-raw-images/pub/ods/surface/sol/00026/ids/edr/browse/shrlc/SI1_0026_0669252912_124ECM_N0030792SRLC07009_0000LUJ02_1200.jpg",
+            "earth_date": "2021-03-16",
+            "rover": {
+                "id": 8,
+                "name": "Perseverance",
+                "landing_date": "2021-02-18",
+                "launch_date": "2020-07-30",
+                "status": "active"
+            }
+        },
+        {
+            "id": 816857,
+            "sol": 26,
+            "camera": {
+                "id": 47,
+                "name": "SHERLOC_WATSON",
+                "rover_id": 8,
+                "full_name": "SHERLOC WATSON Camera"
+            },
+            "img_src": "https://mars.nasa.gov/mars2020-raw-images/pub/ods/surface/sol/00026/ids/edr/browse/shrlc/SI1_0026_0669253495_138ECM_N0030792SRLC07003_0000LUJ02_1200.jpg",
+            "earth_date": "2021-03-16",
+            "rover": {
+                "id": 8,
+                "name": "Perseverance",
+                "landing_date": "2021-02-18",
+                "launch_date": "2020-07-30",
+                "status": "active"
+            }
+        },
+        {
+            "id": 816860,
+            "sol": 26,
+            "camera": {
+                "id": 47,
+                "name": "SHERLOC_WATSON",
+                "rover_id": 8,
+                "full_name": "SHERLOC WATSON Camera"
+            },
+            "img_src": "https://mars.nasa.gov/mars2020-raw-images/pub/ods/surface/sol/00026/ids/edr/browse/shrlc/SI1_0026_0669253636_115ECM_N0030792SRLC07004_0000LUJ02_1200.jpg",
+            "earth_date": "2021-03-16",
+            "rover": {
+                "id": 8,
+                "name": "Perseverance",
+                "landing_date": "2021-02-18",
+                "launch_date": "2020-07-30",
+                "status": "active"
+            }
+        },
+        {
+            "id": 816864,
+            "sol": 26,
+            "camera": {
+                "id": 47,
+                "name": "SHERLOC_WATSON",
+                "rover_id": 8,
+                "full_name": "SHERLOC WATSON Camera"
+            },
+            "img_src": "https://mars.nasa.gov/mars2020-raw-images/pub/ods/surface/sol/00026/ids/edr/browse/shrlc/SI1_0026_0669253775_136ECM_N0030792SRLC07004_0000LUJ02_1200.jpg",
+            "earth_date": "2021-03-16",
+            "rover": {
+                "id": 8,
+                "name": "Perseverance",
+                "landing_date": "2021-02-18",
+                "launch_date": "2020-07-30",
+                "status": "active"
+            }
+        },
+        {
+            "id": 816867,
+            "sol": 26,
+            "camera": {
+                "id": 47,
+                "name": "SHERLOC_WATSON",
+                "rover_id": 8,
+                "full_name": "SHERLOC WATSON Camera"
+            },
+            "img_src": "https://mars.nasa.gov/mars2020-raw-images/pub/ods/surface/sol/00026/ids/edr/browse/shrlc/SI1_0026_0669253923_158ECM_N0030792SRLC07004_0000LUJ02_1200.jpg",
+            "earth_date": "2021-03-16",
+            "rover": {
+                "id": 8,
+                "name": "Perseverance",
+                "landing_date": "2021-02-18",
+                "launch_date": "2020-07-30",
+                "status": "active"
+            }
+        },
+        {
+            "id": 816870,
+            "sol": 26,
+            "camera": {
+                "id": 47,
+                "name": "SHERLOC_WATSON",
+                "rover_id": 8,
+                "full_name": "SHERLOC WATSON Camera"
+            },
+            "img_src": "https://mars.nasa.gov/mars2020-raw-images/pub/ods/surface/sol/00026/ids/edr/browse/shrlc/SI1_0026_0669254065_127ECM_N0030792SRLC07006_0000LUJ02_1200.jpg",
+            "earth_date": "2021-03-16",
+            "rover": {
+                "id": 8,
+                "name": "Perseverance",
+                "landing_date": "2021-02-18",
+                "launch_date": "2020-07-30",
+                "status": "active"
+            }
+        },
+        {
+            "id": 816875,
+            "sol": 26,
+            "camera": {
+                "id": 47,
+                "name": "SHERLOC_WATSON",
+                "rover_id": 8,
+                "full_name": "SHERLOC WATSON Camera"
+            },
+            "img_src": "https://mars.nasa.gov/mars2020-raw-images/pub/ods/surface/sol/00026/ids/edr/browse/shrlc/SI1_0026_0669254211_189ECM_N0030792SRLC07005_0000LUJ02_1200.jpg",
+            "earth_date": "2021-03-16",
+            "rover": {
+                "id": 8,
+                "name": "Perseverance",
+                "landing_date": "2021-02-18",
+                "launch_date": "2020-07-30",
+                "status": "active"
+            }
+        },
+        {
+            "id": 816881,
+            "sol": 26,
+            "camera": {
+                "id": 47,
+                "name": "SHERLOC_WATSON",
+                "rover_id": 8,
+                "full_name": "SHERLOC WATSON Camera"
+            },
+            "img_src": "https://mars.nasa.gov/mars2020-raw-images/pub/ods/surface/sol/00026/ids/edr/browse/shrlc/SI1_0026_0669254766_116ECM_N0030792SRLC07007_0000LUJ02_1200.jpg",
+            "earth_date": "2021-03-16",
+            "rover": {
+                "id": 8,
+                "name": "Perseverance",
+                "landing_date": "2021-02-18",
+                "launch_date": "2020-07-30",
+                "status": "active"
+            }
+        },
+        {
+            "id": 817033,
+            "sol": 26,
+            "camera": {
+                "id": 47,
+                "name": "SHERLOC_WATSON",
+                "rover_id": 8,
+                "full_name": "SHERLOC WATSON Camera"
+            },
+            "img_src": "https://mars.nasa.gov/mars2020-raw-images/pub/ods/surface/sol/00026/ids/edr/browse/shrlc/SI1_0026_0669252767_221ECM_N0030792SRLC07008_0000LUJ01_1200.jpg",
+            "earth_date": "2021-03-16",
+            "rover": {
+                "id": 8,
+                "name": "Perseverance",
+                "landing_date": "2021-02-18",
+                "launch_date": "2020-07-30",
+                "status": "active"
+            }
+        },
+        {
+            "id": 817034,
+            "sol": 26,
+            "camera": {
+                "id": 47,
+                "name": "SHERLOC_WATSON",
+                "rover_id": 8,
+                "full_name": "SHERLOC WATSON Camera"
+            },
+            "img_src": "https://mars.nasa.gov/mars2020-raw-images/pub/ods/surface/sol/00026/ids/edr/browse/shrlc/SI1_0026_0669252925_206ECM_N0030792SRLC07009_0000LUJ01_1200.jpg",
+            "earth_date": "2021-03-16",
+            "rover": {
+                "id": 8,
+                "name": "Perseverance",
+                "landing_date": "2021-02-18",
+                "launch_date": "2020-07-30",
+                "status": "active"
+            }
+        },
+        {
+            "id": 817035,
+            "sol": 26,
+            "camera": {
+                "id": 47,
+                "name": "SHERLOC_WATSON",
+                "rover_id": 8,
+                "full_name": "SHERLOC WATSON Camera"
+            },
+            "img_src": "https://mars.nasa.gov/mars2020-raw-images/pub/ods/surface/sol/00026/ids/edr/browse/shrlc/SI1_0026_0669253508_232ECM_N0030792SRLC07003_0000LUJ02_1200.jpg",
+            "earth_date": "2021-03-16",
+            "rover": {
+                "id": 8,
+                "name": "Perseverance",
+                "landing_date": "2021-02-18",
+                "launch_date": "2020-07-30",
+                "status": "active"
+            }
+        },
+        {
+            "id": 817036,
+            "sol": 26,
+            "camera": {
+                "id": 47,
+                "name": "SHERLOC_WATSON",
+                "rover_id": 8,
+                "full_name": "SHERLOC WATSON Camera"
+            },
+            "img_src": "https://mars.nasa.gov/mars2020-raw-images/pub/ods/surface/sol/00026/ids/edr/browse/shrlc/SI1_0026_0669253649_230ECM_N0030792SRLC07004_0000LUJ01_1200.jpg",
+            "earth_date": "2021-03-16",
+            "rover": {
+                "id": 8,
+                "name": "Perseverance",
+                "landing_date": "2021-02-18",
+                "launch_date": "2020-07-30",
+                "status": "active"
+            }
+        },
+        {
+            "id": 817037,
+            "sol": 26,
+            "camera": {
+                "id": 47,
+                "name": "SHERLOC_WATSON",
+                "rover_id": 8,
+                "full_name": "SHERLOC WATSON Camera"
+            },
+            "img_src": "https://mars.nasa.gov/mars2020-raw-images/pub/ods/surface/sol/00026/ids/edr/browse/shrlc/SI1_0026_0669253788_224ECM_N0030792SRLC07004_0000LUJ02_1200.jpg",
+            "earth_date": "2021-03-16",
+            "rover": {
+                "id": 8,
+                "name": "Perseverance",
+                "landing_date": "2021-02-18",
+                "launch_date": "2020-07-30",
+                "status": "active"
+            }
+        },
+        {
+            "id": 817038,
+            "sol": 26,
+            "camera": {
+                "id": 47,
+                "name": "SHERLOC_WATSON",
+                "rover_id": 8,
+                "full_name": "SHERLOC WATSON Camera"
+            },
+            "img_src": "https://mars.nasa.gov/mars2020-raw-images/pub/ods/surface/sol/00026/ids/edr/browse/shrlc/SI1_0026_0669253936_214ECM_N0030792SRLC07004_0000LUJ01_1200.jpg",
+            "earth_date": "2021-03-16",
+            "rover": {
+                "id": 8,
+                "name": "Perseverance",
+                "landing_date": "2021-02-18",
+                "launch_date": "2020-07-30",
+                "status": "active"
+            }
+        },
+        {
+            "id": 817039,
+            "sol": 26,
+            "camera": {
+                "id": 47,
+                "name": "SHERLOC_WATSON",
+                "rover_id": 8,
+                "full_name": "SHERLOC WATSON Camera"
+            },
+            "img_src": "https://mars.nasa.gov/mars2020-raw-images/pub/ods/surface/sol/00026/ids/edr/browse/shrlc/SI1_0026_0669254079_005ECM_N0030792SRLC07006_0000LUJ01_1200.jpg",
+            "earth_date": "2021-03-16",
+            "rover": {
+                "id": 8,
+                "name": "Perseverance",
+                "landing_date": "2021-02-18",
+                "launch_date": "2020-07-30",
+                "status": "active"
+            }
+        },
+        {
+            "id": 817040,
+            "sol": 26,
+            "camera": {
+                "id": 47,
+                "name": "SHERLOC_WATSON",
+                "rover_id": 8,
+                "full_name": "SHERLOC WATSON Camera"
+            },
+            "img_src": "https://mars.nasa.gov/mars2020-raw-images/pub/ods/surface/sol/00026/ids/edr/browse/shrlc/SI1_0026_0669254226_049ECM_N0030792SRLC07005_0000LUJ01_1200.jpg",
+            "earth_date": "2021-03-16",
+            "rover": {
+                "id": 8,
+                "name": "Perseverance",
+                "landing_date": "2021-02-18",
+                "launch_date": "2020-07-30",
+                "status": "active"
+            }
+        },
+        {
+            "id": 817041,
+            "sol": 26,
+            "camera": {
+                "id": 47,
+                "name": "SHERLOC_WATSON",
+                "rover_id": 8,
+                "full_name": "SHERLOC WATSON Camera"
+            },
+            "img_src": "https://mars.nasa.gov/mars2020-raw-images/pub/ods/surface/sol/00026/ids/edr/browse/shrlc/SI1_0026_0669254780_009ECM_N0030792SRLC07007_0000LUJ02_1200.jpg",
+            "earth_date": "2021-03-16",
+            "rover": {
+                "id": 8,
+                "name": "Perseverance",
+                "landing_date": "2021-02-18",
+                "launch_date": "2020-07-30",
+                "status": "active"
+            }
+        }
+    ]
+}
